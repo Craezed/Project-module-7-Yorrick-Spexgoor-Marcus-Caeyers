e7c6a9b1bdcf563dc432afd960d68a6e64347554
YorrickS <y.spexgoor@student.utwente.nl>
2026-04-07 11:18:08 +0200

    Main template

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import pygame
+
 def main():
-    pass
+
+    size = (1080, 720)  # Set the screen size
+    pygame.init()  # Initialize the pygame library
+    screen = pygame.display.set_mode(size)  # Initialize the pygame screen
+    clock = pygame.time.Clock()  # Initialize a pygame timer
+
+    while True:
+        # Check for pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # If the screen is closed, quit the program
+                pygame.quit()
+
+        screen.fill(0)  # Fill the whole screen with the color black
+
+        pygame.draw.rect(screen, 999999, ([50, 50], [50, 50]))
+
+        pygame.display.flip()  # Update the screen
+
+        clock.tick(60)  # Limits frame rate to 60 FPS
 
 
 
