5348c09721094af05d64aa6b004ac149a391b7f5
YorrickS <y.spexgoor@student.utwente.nl>
2026-04-07 11:54:49 +0200

    Camera

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pygame
+import pygame.camera
 
 def main():
 
     size = (1080, 720)  # Set the screen size
     pygame.init()  # Initialize the pygame library
+    pygame.camera.init()
     screen = pygame.display.set_mode(size)  # Initialize the pygame screen
     clock = pygame.time.Clock()  # Initialize a pygame timer
+
+    camlist = pygame.camera.list_cameras()
+    if not camlist:
+        print("No camera found")
+        pygame.quit()
+
+    cam = pygame.camera.Camera(camlist[0], size)
+    cam.start()
+
+    # Create frame
+    rect = pygame.Rect([0, 0], [200, 200])
+    rect.center = (size[0] // 2, size[1] // 2)
+
+    img = pygame.Surface(size) # To store camera frames
 
     while True:
         # Check for pygame events
@@ -13,11 +29,17 @@ def main():
             if event.type == pygame.QUIT:  # If the screen is closed, quit the program
                 pygame.quit()
 
-        screen.fill(0)  # Fill the whole screen with the color black
+        # Capture image from camera
+        try:
+            cam.get_image(img)
+        except Exception as e:
+            print(f"Error capturing image: {e}")
+            break
 
-        pygame.draw.rect(screen, 999999, ([50, 50], [50, 50]))
+        screen.blit(img, (0, 0))
+        pygame.draw.rect(screen, 999999, rect, 10)
 
-        pygame.display.flip()  # Update the screen
+        pygame.display.flip() # Update Screen
 
         clock.tick(60)  # Limits frame rate to 60 FPS
 
